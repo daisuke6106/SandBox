7de221cca0d796567a50daadc41e61d95f85490f
Daisuke Kanno <daisuke6106@gmail.com>
2015-12-18 01:39:21 +0900

    インデントが気持ち悪い

--- src/Sample.java
+++ src/Sample.java
@@ -10,25 +10,25 @@ public class Sample {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        int n = Integer.parseInt(line);
-        BigDecimal result = new BigDecimal(n--);
-        for (;n>=1; n--) result = result.multiply(new BigDecimal(n));
-        String resultStr = result.toString();
-        
-        Pattern com = Pattern.compile("^([0-9]+?)0*$");
-        Matcher matcher = com.matcher(resultStr);
-        if (matcher.find()) resultStr = matcher.group(1);
-        
-        com = Pattern.compile("^([0-9]+)([0-9]{9})$");
-        matcher = com.matcher(resultStr);
-        if (matcher.find()) resultStr = matcher.group(2);
-        
-        com = Pattern.compile("^0*([0-9]+)$");
-        matcher = com.matcher(resultStr);
-        if (matcher.find()) resultStr = matcher.group(1);
-        
-        System.out.println(resultStr);
+		String line = br.readLine();
+		int n = Integer.parseInt(line);
+		BigDecimal result = new BigDecimal(n--);
+		for (;n>=1; n--) result = result.multiply(new BigDecimal(n));
+		String resultStr = result.toString();
+		
+		Pattern com = Pattern.compile("^([0-9]+?)0*$");
+		Matcher matcher = com.matcher(resultStr);
+		if (matcher.find()) resultStr = matcher.group(1);
+		
+		com = Pattern.compile("^([0-9]+)([0-9]{9})$");
+		matcher = com.matcher(resultStr);
+		if (matcher.find()) resultStr = matcher.group(2);
+		
+		com = Pattern.compile("^0*([0-9]+)$");
+		matcher = com.matcher(resultStr);
+		if (matcher.find()) resultStr = matcher.group(1);
+		
+		System.out.println(resultStr);
 	}
 
 }
